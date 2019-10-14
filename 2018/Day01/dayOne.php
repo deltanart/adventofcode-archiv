f5da0bd9ee05bb38e329cdec6c74736972db6522
DeltaNart <c.neinert@outlook.com>
2019-10-14 16:39:39 +0200

    second part
    did i not understand the task?

--- 2018/Day01/dayOne.php
+++ 2018/Day01/dayOne.php
@@ -1,12 +1,28 @@
 <?php
 $handle = fopen("input.txt", "r")or die ("Unable to read the input file");
 $result = 0;
+$findDoubles = array();
+$found = 0;
+for ($index = 0; $index<100000; $index++){
+    while (($line = fgets($handle)) !== false) {
+        // process the line read.
+        $result += $line;
 
-while (($line = fgets($handle)) !== false) {
-    // process the line read.
-    $result += $line;
+        foreach ($findDoubles as $findDouble) {
+
+            if ($findDouble == $result){
+                echo $result." ";
+            }
+        }
+
+        array_push($findDoubles, $result);
+
+    }
 }
+
 fclose($handle);
-echo $result;
+echo "Sum: ".$result."<br><br>";
+
+
 
 
