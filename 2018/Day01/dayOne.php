6793be407384b288a820f1e5ae9056d58f905dab
DeltaNart <c.neinert@outlook.com>
2019-10-14 11:42:42 +0200

    first part

--- 2018/Day01/dayOne.php
+++ 2018/Day01/dayOne.php
@@ -1,1 +1,12 @@
 <?php
+$handle = fopen("input.txt", "r")or die ("Unable to read the input file");
+$result = 0;
+
+while (($line = fgets($handle)) !== false) {
+    // process the line read.
+    $result += $line;
+}
+fclose($handle);
+echo $result;
+
+
