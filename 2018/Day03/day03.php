40bc6602dc5ba62a72223d2220dfd9a0750cf3b9
DeltaNart <c.neinert@outlook.com>
2019-10-15 16:05:34 +0200

    day3 init

---
+++ 2018/Day03/day03.php
@@ -0,0 +1,13 @@
+#!/usr/bin/php
+<?php
+function filereader($infile){
+    //Function that returns the contents of a file and stores it line by line in an array
+    $array = array();
+    $handle = fopen($infile, "r") or die ("Unable to open the requested File");
+    while ($line = fgets($handle)){
+        $array[]= $line;
+    }
+    fclose($handle);
+    return $array;
+}
+
