f8a587a964a29cb2d317d58b37af2fa00ba5b922
DeltaNart <c.neinert@outlook.com>
2019-10-16 14:07:13 +0200

    created functions that should handle every step of the precess
    the testInput is for testing purposes: expected value is 4 at the end

--- 2018/Day03/day03.php
+++ 2018/Day03/day03.php
@@ -10,4 +10,56 @@ function filereader($infile){
     fclose($handle);
     return $array;
 }
+function inputhandler($RawInput){
+    foreach ($RawInput as $line) {
+        $line = preg_match_all('/(\d+)/',$line,$temp);
+        $returner[] = $temp[0];
+    }
+    return $returner;
+}
 
+function findOverlapPoint($RawArray){
+    //return a list of x and y coordinates
+    //each pair represents a point that overlaps with any other
+    //Input Variables
+    foreach ($RawArray as $item) {
+        $arrayX[] = $item[1];//Array of all X Values
+        $arrayRangeX[] = $item[3];//Array of all ranges for X
+        $arrayY[] = $item[2];
+        $arrayRangeY[] = $item[4];
+    }
+    print_r($arrayX);
+    //temps
+    $foundX = array();
+    $foundY = array();
+    //Logic
+    $indexWhile = 0;
+    while ($indexWhile < count($arrayX)){
+        for ($innerIndex = 0; $innerIndex<=count($arrayX);$innerIndex+=1){
+            if ($arrayX[$indexWhile] > $arrayX[$innerIndex] and $arrayX[$indexWhile]+$arrayRangeX[$indexWhile] < $arrayX[$innerIndex]+$arrayRangeX[$innerIndex]){
+                if ($arrayY[$indexWhile] > $arrayY[$innerIndex] and $arrayY[$indexWhile]+$arrayRangeY[$indexWhile] < $arrayY[$innerIndex]+$arrayRangeY[$innerIndex]){
+                    $foundX[] = $arrayX[$indexWhile];
+                    $foundY[] = $arrayY[$indexWhile];
+                }
+            }
+        }
+        $indexWhile+=1;
+    }
+    print_r($foundX);
+    print_r($foundY);
+
+
+
+    //prepare Output
+    $returnArray = array();
+    for ($index = 0; $index <= count($foundX); $index += 1 ){
+        $returnArray[] = array($foundX[$index], $foundY[$index]);
+    }
+    return array();
+}
+
+function deleteDoubles($arrayOfAllPoints){
+    return array_unique($arrayOfAllPoints);
+}
+
+echo findOverlapPoint(inputhandler(filereader("testInput.txt")));
