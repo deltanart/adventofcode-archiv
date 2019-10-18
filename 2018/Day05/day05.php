958a01c834ddb67e337c832bf5fb10b59be941ab
DeltaNart <c.neinert@outlook.com>
2019-10-18 10:13:15 +0200

    added the input

--- 2018/Day05/day05.php
+++ 2018/Day05/day05.php
@@ -5,11 +5,9 @@ function filereader($infile){
 //Function that returns the contents of a file and stores it line by line in an array
 $array = array();
 $handle = fopen($infile, "r") or die ("Unable to open the requested File");
-while ($line = fgets($handle)){
-$array[]= $line;
-}
+$line = fgets($handle);
 fclose($handle);
-return $array;
+return $line;
 }
 
 
