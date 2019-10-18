b597028ea8cc159eb1e02094391bb5e52abf75d8
DeltaNart <c.neinert@outlook.com>
2019-10-18 13:17:25 +0200

    the loop gets cut off too early

--- 2018/Day05/day05.php
+++ 2018/Day05/day05.php
@@ -7,25 +7,48 @@ $array = array();
 $handle = fopen($infile, "r") or die ("Unable to open the requested File");
 $line = fgets($handle);
 fclose($handle);
+$line = str_split($line,1); //line is now an array of each character
 return $line;
 }
 
-
-function inputhandler($RawInput){
-$data = array();
-foreach ($RawInput as $line) {
-if(preg_match_all("/\[\d+-\d+-\d+\s\d+:\d+\]/", $line, $key)); //Regex für den Zeitstempel in den [ ] Klammern
-if(preg_match_all("/\d:\d+](\s*\w*)*(#\d+)*/", $line, $dataentry)); //Regex für alles hinter dem Zeitstempel
-$data[$key[0][0]] = $dataentry[0][0];
-}
-ksort($data);
-return $data;
+function isEqual($charOne, $charTwo){
+    //checks to chars regardles of upper or lower case if there are identical and returns a bool
+    $charOne = strtolower($charOne);
+    $charTwo = strtolower($charTwo);
+    if ($charOne == $charTwo){
+        return TRUE;
+    }else{ return FALSE;}
 }
 
-
-
-
-
-
-function part01(){}
+function part01($array){
+    $foundCase = TRUE;
+    while($foundCase) {
+        $foundCases = 0;
+        foreach ($array as $key => $item) {
+            if (array_key_exists ($key , $array)){
+                if ($key === 0) {
+                    continue;
+                }
+                if (isEqual($array[$key - 1], $array[$key])) {
+                    unset($array[$key], $array[$key - 1]);
+                    $array = array_values($array);
+                    $foundCases += 1;
+                }
+                else {
+                    if ($foundCases<1) {
+                        $foundCases = FALSE;
+                    }
+                }
+            }else{
+                break;
+            }
+        }
+        if (!$foundCases){
+            $foundCase = FALSE;
+        }
+    }
+    return $array;
+}
 function part02(){}
+
+print_r(part01(filereader('testInput.txt')));
