87b214665c1ca438e18c089227aef0ac87199197
DeltaNart <c.neinert@outlook.com>
2019-10-21 09:57:09 +0200

    test of new function that should return a value as positive regardless if it is negative or not

--- 2018/Day06/day06.php
+++ 2018/Day06/day06.php
@@ -20,13 +20,38 @@ function filereader($infile){
 }
 
 function absoluteValue($integer){
-    if ($integer < 0){
-        return $integer *(-1);
-    }else{
-        return $integer;
+    if (is_int($integer)){
+        if ($integer < 0){
+            return $integer *(-1);
+        }else{
+            return $integer;
+        }
     }
+    return "Fatal Error: The value is not in Integer in the absoluteValue-Function\n";
+
 }
 
+
+function cityMetric($ArrayPoint_A, $ArrayPoint_B){
+    $delta = absoluteValue($ArrayPoint_A[0] - $ArrayPoint_B[0]) + absoluteValue($ArrayPoint_A[1] - $ArrayPoint_B[1]);
+    return $delta;
+}
+//print_r(filereader("testInput.txt"));
+
+
+
+
+
+
+
+
+
+//*******  TESTS ******
+
+/**
+ * @return array
+ * the array contains a string of each test case and a bool of the testpass;
+ */
 function testAbsuluteValue(){
     $tests = array();
     if (absoluteValue(0) === 0){$tests[] = "Test01: TRUE\n";}else{$tests[] ="Test01: FALSE\n";}
@@ -36,14 +61,13 @@ function testAbsuluteValue(){
     if (absoluteValue(-27) === 27){$tests[] = "Test05: TRUE\n";}else{$tests[] ="Test05: FALSE\n";}
     if (absoluteValue(555) === 555){$tests[] = "Test06: TRUE\n";}else{$tests[] ="Test06: FALSE\n";}
     if (absoluteValue(-232) === 232){$tests[] = "Test07: TRUE\n";}else{$tests[] ="Test07: FALSE\n";}
+    if (absoluteValue("Test") === "Fatal Error: The value is not in Integer in the absoluteValue-Function\n"){$tests[] = "Test08: TRUE\n";}else{$tests[] ="Test08: FALSE\n";}
+
+
     return $tests;
 }
 
 
-function cityMetric($ArrayPoint_A, $ArrayPoint_B){
-    $delta = 0;
+function test_cityMetric(){
+
 }
-print_r(filereader("testInput.txt"));
-
-
-print_r(testAbsuluteValue());
