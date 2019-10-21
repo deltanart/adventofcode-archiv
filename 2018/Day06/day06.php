ad53f7be8b5a250bc50759d173153b54cafed700
DeltaNart <c.neinert@outlook.com>
2019-10-21 09:37:48 +0200

    test of new function that should return a value as positive regardless if it is negative or not

--- 2018/Day06/day06.php
+++ 2018/Day06/day06.php
@@ -19,9 +19,31 @@ function filereader($infile){
     return $array;
 }
 
+function absoluteValue($integer){
+    if ($integer < 0){
+        return $integer *(-1);
+    }else{
+        return $integer;
+    }
+}
+
+function testAbsuluteValue(){
+    $tests = array();
+    if (absoluteValue(0) === 0){$tests[] = "Test01: TRUE\n";}else{$tests[] ="Test01: FALSE\n";}
+    if (absoluteValue(-0) === 0){$tests[] = "Test02: TRUE\n";}else{$tests[] ="Test02: FALSE\n";}
+    if (absoluteValue(1) === 1){$tests[] = "Test03: TRUE\n";}else{$tests[] ="Test03: FALSE\n";}
+    if (absoluteValue(-1) === 1){$tests[] = "Test04: TRUE\n";}else{$tests[] ="Test04: FALSE\n";}
+    if (absoluteValue(-27) === 27){$tests[] = "Test05: TRUE\n";}else{$tests[] ="Test05: FALSE\n";}
+    if (absoluteValue(555) === 555){$tests[] = "Test06: TRUE\n";}else{$tests[] ="Test06: FALSE\n";}
+    if (absoluteValue(-232) === 232){$tests[] = "Test07: TRUE\n";}else{$tests[] ="Test07: FALSE\n";}
+    return $tests;
+}
 
 
 function cityMetric($ArrayPoint_A, $ArrayPoint_B){
     $delta = 0;
 }
 print_r(filereader("testInput.txt"));
+
+
+print_r(testAbsuluteValue());
