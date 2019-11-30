26489b7f694062925020c7891a0321e980f2ba15
DeltaNart <c.neinert@outlook.com>
2019-11-30 21:23:58 +0100

    init and encryption

--- 2019/Day1/day01.php
+++ 2019/Day1/day01.php
@@ -9,3 +9,21 @@ function prepareInput($infile){
     preg_match_all("/\d+/",$array[0],$array);
     return $array[0];
 }
+
+function encrypter($Value, $Salt){
+    return sha1($Salt . $Value . $Salt);
+}
+
+function is_Correct_Hash($Hash, $Value, $Salt){
+    return $Hash === encrypter($Value, $Salt) ? TRUE : FALSE;
+}
+
+$Value = "MySecretString";
+$Salt = "SuperSalty";
+
+$Hash = encrypter($Value, $Salt);
+if(is_Correct_Hash($Hash, $Value, $Salt)){
+    echo "True\n";
+}else{
+    echo "False\n";
+} 
