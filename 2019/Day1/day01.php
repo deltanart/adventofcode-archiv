c7858ba90b63245b76a90424fe5aeb385e37a4c7
DeltaNart <c.neinert@outlook.com>
2019-12-01 10:18:09 +0100

    day one part one and two added

--- 2019/Day1/day01.php
+++ 2019/Day1/day01.php
@@ -1,29 +1,47 @@
 #!/usr/bin/php
 <?php
 
-function prepareInput($infile){
+function filereader($inputfile){
     //Function that returns the contents of a file and stores it line by line in an array
-    $handle = fopen($infile, "r") or die ("Unable to open the requested File");
-    $array[] = fgets($handle);
+    $array = array();
+    $handle = fopen($inputfile, "r") or die ("Unable to open the requested File");
+    while ($line = fgets($handle)){
+        array_push($array, $line);
+    }
     fclose($handle);
-    preg_match_all("/\d+/",$array[0],$array);
-    return $array[0];
+    return $array;
 }
 
-function encrypter($Value, $Salt){
-    return sha1($Salt . $Value . $Salt);
+function fuelPerModule($Mass){
+    return (floor($Mass/3) -2);
 }
 
-function is_Correct_Hash($Hash, $Value, $Salt){
-    return $Hash === encrypter($Value, $Salt) ? TRUE : FALSE;
+function d1p1($InputArray){
+    $fuelparts = array();
+    foreach ($InputArray as $Input){
+        $fuelparts[] = fuelPerModule($Input);
+    }
+    return $fuelparts;
 }
 
-$Value = "MySecretString";
-$Salt = "SuperSalty";
+function d1p2($InputMassArray){
+    $MassPerModule = array();
+    foreach ($InputMassArray as $InputMass){
+        $Mass = $InputMass;
+        $MassArray = array($Mass);
+        while(fuelPerModule($Mass) > 0){
+            $Mass = fuelPerModule($Mass);
+            $MassArray[] = $Mass; 
+        }
+        $MassPerModule[] = array_sum($MassArray);
+    }
+    return $MassPerModule;
+    
+}
+echo "Day 1 Part 1/2:\n";
 
-$Hash = encrypter($Value, $Salt);
-if(is_Correct_Hash($Hash, $Value, $Salt)){
-    echo "True\n";
-}else{
-    echo "False\n";
-} 
+echo array_sum(d1p1(filereader("input.txt"))) . "\n\n";
+
+echo "Day 1 Part 2/2:\n";
+
+echo array_sum(d1p2(d1p1(filereader("input.txt")))) . "\n\n";
